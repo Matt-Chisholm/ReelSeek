f658b43f395fc6247b9e3ae1f12f0686540b5d15
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-04-19 10:55:00 -0700

    fixed darkmode button

--- src/components/Home.js
+++ src/components/Home.js
@@ -44,33 +44,13 @@ export default function Home() {
             className='w-6 h-6 rounded-full cursor-pointer'
             onClick={handleModeChange}>
             {isDarkMode ? (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'>
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M6 18L18 6M6 6l12 12'
-                />
-              </svg>
+              <span className='text-gray-600 hover:text-white cursor-pointer'>
+                Light mode
+              </span>
             ) : (
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                className='h-6 w-6'
-                fill='none'
-                viewBox='0 0 24 24'
-                stroke='currentColor'>
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M12 6v.01M18 10.5l-6-6M6 10.5l6-6M12 18v.01M6 13.5h12'
-                />
-              </svg>
+              <span className='text-gray-400 hover:text-gray-800 cursor-pointer'>
+                Dark mode
+              </span>
             )}
           </div>
         </div>
