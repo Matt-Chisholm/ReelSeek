c7510ec7e58e2ea81c279dc4ee6bd77e1f8cd0b9
Matt-Chisholm <mathesonchisholm@hotmail.com>
2023-04-25 11:56:53 -0700

    strokeWidth typo

--- src/components/Home.js
+++ src/components/Home.js
@@ -142,7 +142,7 @@ export default function Home() {
                     cy='12'
                     r='10'
                     stroke='currentColor'
-                    stroke-width='4'></circle>
+                    strokeWidth='4'></circle>
                   <path
                     className='opacity-75'
                     fill='currentColor'
